7fde25e3b9c637d5664fc0d8053d47756849e538
Gopi chandu <gopichandupaladugu1@gmail.com>
2025-11-25 18:26:29 +0530

    added cookies

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 const mysql = require('mysql2');
 const bcrypt = require('bcrypt');
+const cookieparser = require('cookie-parser');
 //importing modules
 
 const app=express();
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(cookieparser());
 //linking html public folder
 app.use(express.static(path.join(__dirname,"public")));
 
@@ -31,6 +33,10 @@ db.connect(err=>{
 app.post("/signup",async (req,res)=>{
 
     const {username,email,password}=req.body;
+    if(!username || !email || !password ){
+        return res.status(400).json({ success: false, message: "All fields are required" });
+    }
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+={}[\]|\\;:'",.<>/?]).{8,}$/;
     try{
         const hashedpassword = await bcrypt.hash(password,10);
         const query = "INSERT INTO users (username,email,password) VALUES (?,?,?)";
@@ -38,14 +44,12 @@ app.post("/signup",async (req,res)=>{
         if(err){
             console.log("Eroor ",err);
             return res.send("Signup Failed")
-        }
+        }else{
         return res.send("Signup Succesfull");
-
+        }
     })
     console.log("Signup recieved");
     console.log(username,email,password);
-
-    return res.send("Sign up succesfull :");
 
     }
     catch{
@@ -56,36 +60,43 @@ app.post("/signup",async (req,res)=>{
 })
 
 //login
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    const query = "SELECT * FROM users WHERE email = ?";
 
-app.post("/login",async (req,res)=>{
-    const {email,password}=req.body;
-    const query = "SELECT * FROM users where email = ?";
-    db.query(query,[email],async (err,result)=>{
-        if(err){
-           return  res.send("Login Error");
+    db.query(query, [email], async (err, result) => {
+        if (err) {
+            return res.json({ success: false, message: "Login Error" });
         }
-        if(result.length === 0) {
-            return res.send("User Not Found: ")
+
+        if (result.length === 0) {
+            return res.json({ success: false, message: "User Not Found" });
         }
-        console.log(result)
+
         const user = result[0];
-        const ismatch = await bcrypt.compare(password,user.password);
-        if(ismatch){
-         return res.json({
-        success:true,
-        username:user.username,
-        message:"Login Succesfull"
-       })
+        const ismatch = await bcrypt.compare(password, user.password);
+
+        if (ismatch) {
+            res.cookie("session-user", user.username, {
+                httpOnly: false,
+                secure: false,
+                maxAge: 1000 * 60 * 60
+            });
+
+            return res.json({
+                success: true,
+                username: user.username,
+                message: "Login Successful"
+            });
         }
-        else{
-            return res.send("Wrong username or password : ")
-        }
-       
-    })
-    console.log("Login REcieved :")
-    console.log(email,password)
-   
-})
+
+        return res.json({
+            success: false,
+            message: "Wrong username or password"
+        });
+    });
+});
+
 
 app.listen(3001,()=>{
 console.log(`Server running at http://localhost:3001`)
